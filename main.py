56a86a13e1b7ed3cb90e0aed7e8f2dc6ff043d38
Sphesihle Mhlongo <120335426+sphesihlemhlongo@users.noreply.github.com>
2025-10-25 00:29:13 +0200

    Vercel

--- main.py
+++ main.py
@@ -115,5 +115,5 @@ def get_chat_history():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 9000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
